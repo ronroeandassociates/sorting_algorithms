598ff4f9793a98f9e710a6546e8c8ca7cb2404d2
Ronroeandassociates <3321@holbertonschool.com>
2022-02-04 12:48:09 -0600

    did I make betty happy

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -11,36 +11,36 @@
 
 void merge(int *array, int low, int middle, int high, int *temp)
 {
-  int i, j, k, l = 0, r = 0, n, left[4096], right[4096];
+	int i, j, k, l = 0, r = 0, n, left[4096], right[4096];
 
-  printf("Merging...\n");
-  i = low, j = middle + 1, k = l = 0;
-  while (i <= middle && j <= high)
-  {
-    if (array[i] <= array[j])
-      temp[k] = left[l] = array[i], k++, i++, l++;
-    else
-      temp[k] = right[r] = array[j], k++, j++, r++;
-  }
-  while (i <= middle)
-    temp[k] = left[l] = array[i], k++, i++, l++;
-  while (j <= high)
-    temp[k] = right[r] = array[j], k++, j++, r++;
-  printf("[left]: ");
-  for (n = 0; n < l; n++)
-    (n == 0) ? printf("%d", left[n]) : printf(", %d", left[n]);
-  printf("\n[right]: ");
-  for (n = 0; n < r; n++)
-    (n == 0) ? printf("%d", right[n]) : printf(", %d", right[n]);
-  printf("\n[Done]: ");
-  for (i = low; i <= high; i++)
-  {
-    array[i] = temp[i - low], printf("%d", array[i]);
-    if (i != high)
-      printf(", ");
-    else
-      printf("\n");
-  }
+	printf("Merging...\n");
+	i = low, j = middle + 1, k = l = 0;
+	while (i <= middle && j <= high)
+	{
+		if (array[i] <= array[j])
+			temp[k] = left[l] = array[i], k++, i++, l++;
+		else
+			temp[k] = right[r] = array[j], k++, j++, r++;
+	}
+	while (i <= middle)
+		temp[k] = left[l] = array[i], k++, i++, l++;
+	while (j <= high)
+		temp[k] = right[r] = array[j], k++, j++, r++;
+	printf("[left]: ");
+	for (n = 0; n < l; n++)
+		(n == 0) ? printf("%d", left[n]) : printf(", %d", left[n]);
+	printf("\n[right]: ");
+	for (n = 0; n < r; n++)
+		(n == 0) ? printf("%d", right[n]) : printf(", %d", right[n]);
+	printf("\n[Done]: ");
+	for (i = low; i <= high; i++)
+	{
+		array[i] = temp[i - low], printf("%d", array[i]);
+		if (i != high)
+			printf(", ");
+		else
+			printf("\n");
+	}
 }
 
 /**
@@ -53,15 +53,15 @@ void merge(int *array, int low, int middle, int high, int *temp)
 
 void merge_sort_y(int *array, int low, int high, int *temp)
 {
-  int middle;
+	int middle;
 
-  if (low < high)
-  {
-    middle = ((high + low - 1) / 2);
-    merge_sort_y(array, low, middle, temp);
-    merge_sort_y(array, middle + 1, high, temp);
-    merge(array, low, middle, high, temp);
-  }
+	if (low < high)
+	{
+		middle = ((high + low - 1) / 2);
+		merge_sort_y(array, low, middle, temp);
+		merge_sort_y(array, middle + 1, high, temp);
+		merge(array, low, middle, high, temp);
+	}
 }
 
 /**
@@ -72,13 +72,13 @@ void merge_sort_y(int *array, int low, int high, int *temp)
 
 void merge_sort(int *array, size_t size)
 {
-  int *temp;
+	int *temp;
 
-  if (array == NULL || size < 2)
-    return;
-  temp = malloc(sizeof(int) * (size + 1));
-  if (temp == NULL)
-    return;
-  merge_sort_y(array, 0, size - 1, temp);
-  free(temp);
+	if (array == NULL || size < 2)
+		return;
+	temp = malloc(sizeof(int) * (size + 1));
+	if (temp == NULL)
+		return;
+	merge_sort_y(array, 0, size - 1, temp);
+	free(temp);
 }
